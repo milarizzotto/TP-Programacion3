6b32c6ada1ebeb178fafa4efff8a7ea639be1fec
Tomas Geuna <tomasignaciogeuna@gmail.com>
2024-04-25 15:53:49 -0300

    Agregados los cambios en los algoritmos de busqueda ucs y gbfs.

--- tp-pathfinding/src/pathfinder/search/gbfs.py
+++ tp-pathfinding/src/pathfinder/search/gbfs.py
@@ -3,6 +3,12 @@ from ..models.frontier import PriorityQueueFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
 
+#Definir una funcion heuristica
+def heuristica(node, objetivo):
+    distancia_x = abs(node.state[0] - objetivo[0])
+    distancia_y = abs(node.state[1] - objetivo[1])
+    distancia = distancia_x + distancia_y
+    return distancia
 
 class GreedyBestFirstSearch:
     @staticmethod
@@ -24,4 +30,31 @@ class GreedyBestFirstSearch:
         # Add the node to the explored dictionary
         explored[node.state] = True
         
-        return NoSolution(explored)
+        # Initialize the frontier with the initial node
+        # In this example, the frontier is a Priority Queue Frontier
+        frontier = PriorityQueueFrontier()
+        frontier.add(node, heuristica(node, grid.end)) #grid.end es la posicion objetivo
+
+        while True:
+            
+            #  Fail if the frontier is empty
+
+            if frontier.is_empty():
+                return NoSolution(explored)
+            
+            # Remove a node from the frontier
+            node = frontier.pop()
+
+            if node.state == grid.end:
+                return Solution(node, explored)
+
+            for action in grid.get_neighbours(node.state).items():
+                new_state = action[1]
+                cost = node.cost + grid.get_cost(new_state)
+                if new_state not in explored or cost < explored[new_state]:
+                    new_node = Node("",new_state,cost)
+                    explored[new_state] = cost
+                    frontier.add(new_node,heuristica(new_node, grid.end))
+
+            
+        
--- tp-pathfinding/src/pathfinder/search/ucs.py
+++ tp-pathfinding/src/pathfinder/search/ucs.py
@@ -1,5 +1,5 @@
 from ..models.grid import Grid
-from ..models.frontier import StackFrontier
+from ..models.frontier import PriorityQueueFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
 
@@ -11,7 +11,7 @@ class UniformCostSearch:
 
         Args:
             grid (Grid): Grid of points
-
+            
         Returns:
             Solution: Solution found
         """
@@ -22,6 +22,28 @@ class UniformCostSearch:
         explored = {} 
         
         # Add the node to the explored dictionary
-        explored[node.state] = True
-        
-        return NoSolution(explored)
+        explored[node.state] = 0
+
+        # Initialize the frontier with the initial node
+        # In this example, the frontier is a PriorityQueue
+        frontier = PriorityQueueFrontier()
+        frontier.add(node,0)
+
+        while True:
+
+            #  Fail if the frontier is empty
+            if frontier.is_empty():
+                return NoSolution(explored)
+
+            # Remove a node from the frontier
+            node = frontier.pop()
+            if grid.end == node.state:
+                return Solution(node,explored)
+            # Move
+            for action in grid.get_neighbours(node.state).items():
+                new_state = action[1]
+                cost = node.cost + grid.get_cost(new_state)
+                if new_state not in explored or cost < explored[new_state]:
+                    new_node = Node("",new_state,cost,node,action[0])
+                    explored[new_node.state] = cost
+                    frontier.add(new_node,cost)
